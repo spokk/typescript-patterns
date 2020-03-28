958726df4cee515744205964927f70f83e046d57
Sviatoslav Bondar <sviatoslav_bondar@epam.com>
2020-03-28 21:20:48 +0200

    Proxy

---
+++ proxy.ts
@@ -0,0 +1,41 @@
+interface Object {
+	do(): void;
+}
+
+class ExpensiveObject implements Object {
+	constructor() {
+		console.log('5 sec');
+	}
+	do(): void {
+		console.log('ExpensiveObject do');
+	}
+}
+
+class ExpensiveObjectUser {
+	public object;
+	constructor(object: Object) {
+		this.object = object;
+	}
+
+	do(): void {
+		this.object.do();
+	}
+}
+
+class MyProxy implements Object {
+	public expObject: ExpensiveObject;
+
+	do(): void {
+		console.log('MyProxy');
+
+		if (!this.expObject) {
+			this.expObject = new ExpensiveObject();
+		}
+
+		this.expObject.do();
+	}
+}
+
+const expObjectUser = new ExpensiveObjectUser(new MyProxy());
+expObjectUser.do();
+expObjectUser.do();
